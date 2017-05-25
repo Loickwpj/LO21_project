98e2c5ff18747d7fa6d936628fd78d2bd83c1027
loickwpj <loick.mpemba@gmail.com>
2017-05-25 17:09:08 +0200

    Ajoute Note.cpp

---
+++ Note.cpp
@@ -0,0 +1,42 @@
+#include "Note.h"
+
+/**************************************************************
+***                        Clone                            ***
+***************************************************************/
+
+Tache* Tache::clone(){return new Tache(*this);}
+Article* Article::clone(){return new Article(*this);}
+Image* Image::clone(){return new Image(*this);}
+Audio* Audio::clone(){return new Audio(*this);}
+Video* Video::clone(){return new Video(*this);}
+
+/**************************************************************
+ ***                        Relation                        ***
+ ***************************************************************/
+
+void Relation::addCouple(Note* n1, Note* n2){
+    if (nbCouple == nbMaxCouple ){
+        Couple** newCouples = new Couple* [nbMaxCouple+5];
+        for (unsigned int i=0; i<nbMaxCouple; i++){
+            newCouples[i]= new Couple (*couples[i]);
+        }
+        nbMaxCouple+=5;
+        Couple** oldcouples=couples;
+        couples=newCouples;
+        delete [] oldcouples;
+    }
+    couples[nbCouple++]= new Couple("",n1,n2);
+}
+
+
+void Relation::removeCouple(const string& label, Note* n1, Note* n2){
+    unsigned int i=0;
+    while(i<nbCouple && !(couples[i]->getLabel() == label) && couples[i]->getNote1() == *n1  && couples[i]->getNote1() == *n1) ){
+        i++;
+    }
+    if (i == nbCouple) throw NotesException("Couple inexistant...");
+    
+    delete couples [i];
+    if(i != nbCouple) tags[i]=tags[nbCouple-1];
+    nbCouple--;
+}
